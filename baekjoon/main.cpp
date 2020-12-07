6ae8652b542dfb290e344525d762ad8b59a2f751
nomelancholy <starmekey@naver.com>
2020-12-07 21:40:52 +0900

    add baekjoon structure

---
+++ baekjoon/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "solutions.h"
+
+using namespace std;
+
+int main(){
+    hello_world_2557();
+    return 0;
+}
---
+++ baekjoon/solutions.h
@@ -0,0 +1,6 @@
+#ifndef SOLUTIONS_H_INCLUDED
+#define SOLUTIONS_H_INCLUDED
+
+void hello_world_2557();
+
+#endif // SOLUTIONS_H_INCLUDED
