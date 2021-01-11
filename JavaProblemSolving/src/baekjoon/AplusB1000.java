102c86e0590dcd1e4abf5135c13879ce79b8f95c
TakeKnowledge <starmekey@naver.com>
2021-01-11 22:56:25 +0900

    baekjoon - 1000번. A+B

---
+++ JavaProblemSolving/src/baekjoon/AplusB1000.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class AplusB1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a+b);
+    }
+}
