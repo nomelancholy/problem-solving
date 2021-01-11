7d5a7ec2848ba0f99be7400b3fab6dc6802718c3
nomelancholy <starmekey@naver.com>
2021-01-11 17:30:16 +0900

    baekjoon - 2753번. 윤년

--- c++-problem-solving/baekjoon/exam-grades-9498.cpp
+++ c++-problem-solving/baekjoon/exam-grades-9498.cpp
@@ -6,15 +6,15 @@ using namespace std;
 void exam_grades_9498() {
     int n;
     cin >> n;
-    if(n <= 100 && n >= 90){
+    if(n <= 100 && n >= 90) {
         cout << "A";
-    }else if(n >= 80){
+    } else if(n >= 80) {
         cout << "B";
-    }else if(n >= 70){
+    } else if(n >= 70) {
         cout << "C";
-    }else if(n >= 60){
+    } else if(n >= 60) {
         cout << "D";
-    }else{
+    } else {
         cout << "F";
     }
 }
