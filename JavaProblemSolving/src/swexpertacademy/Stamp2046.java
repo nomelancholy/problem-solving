d5b46231e5dbe7e267e62d7a2093e077e9492ca4
TakeKnowledge <starmekey@naver.com>
2021-01-21 21:35:27 +0900

    swexpertacademy - 2046번. 스탬프 찍기

---
+++ JavaProblemSolving/src/swexpertacademy/Stamp2046.java
@@ -0,0 +1,15 @@
+package swexpertacademy;
+
+import java.util.Scanner;
+
+public class Stamp2046 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for(int i=0; i<n; i++){
+            System.out.print("#");
+        }
+    }
+}
